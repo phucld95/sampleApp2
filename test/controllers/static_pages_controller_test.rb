028d6d6ef3cc794e2142ea2c3be603e51769c42d
“LePhuc” <lephuc@Les-MacBook-Pro.local>
2017-01-04 11:12:39 +0700

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
   test "should get root" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
   end
 
