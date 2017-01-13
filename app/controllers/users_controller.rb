e203e54e467d982107946b12267e70bfa9084306
“LePhuc” <lephuc@Les-MacBook-Pro.local>
2017-01-13 08:44:46 +0700

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create  
+  def create
     @user = User.new user_params
     if @user.save
       @user.send_activation_email
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @microposts = @user.microposts.paginate page: params[:page]
   end
 
   def edit
